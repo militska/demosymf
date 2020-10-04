c840c3ee3ba8991882df78b865c61b1a73006ca2
militska <militska@intersvyaz.net>
2020-10-04 22:06:44 +0500

    add  form

---
+++ src/Form/Type/UserType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+
+/**
+ * Class UserType
+ * @package App\Form\Type
+ */
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('full_name', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('description', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('password', PasswordType::class, ['attr' => ['class' => 'form-control']])
+            ->add('Register', SubmitType::class, ['attr' => ['class' => 'btn btn-primary']]);
+
+    }
+}
